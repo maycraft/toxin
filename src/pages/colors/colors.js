fa1bba25e9147407833062c280c5a456bf733dc8
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-07-29 22:21:05 +0300

    Removed unnecessary code

--- src/pages/colors/colors.js
+++ src/pages/colors/colors.js
@@ -1,9 +1,1 @@
 import './colors.scss';
-
-// import '../../components/button/button';
-// import '../../components/textfield/textfield';
-// import '../../components/checkbox/checkbox';
-// import '../../components/radio/radio';
-// import '../../components/pagination/pagination';
-// import '../../components/toggle/toggle';
-// import '../../components/like/like';
