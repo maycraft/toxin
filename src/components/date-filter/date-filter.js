a728c6b2d364f4e47cca551d279c091077d407d9
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-08-05 14:27:49 +0300

    Added date-filter component

---
+++ src/components/date-filter/date-filter.js
@@ -0,0 +1,46 @@
+import $ from 'jquery';
+import 'air-datepicker';
+import 'air-datepicker/dist/css/datepicker.min.css';
+import './date-filter.scss';
+
+const options = {
+    range: true,
+    multipleDatesSeparator: ' - ',
+    language: {
+        monthsShort: [
+            'янв',
+            'фев',
+            'мар',
+            'апр',
+            'май',
+            'июн',
+            'июл',
+            'авг',
+            'сен',
+            'окт',
+            'ноя',
+            'дек',
+        ],
+    },
+    dateFormat: 'dd M',
+    clearButton: true,
+    prevHtml: '<i class="material-icons date-filter__arrow">arrow_back</i>',
+    nextHtml: '<i class="material-icons date-filter__arrow">arrow_forward</i>',
+    navTitles: {
+        days: 'MM yyyy',
+    },
+    offset: 20,
+};
+
+const $dateFilterInput = $('.date-filter__input');
+const Datepicker = $dateFilterInput.datepicker(options).data('datepicker');
+const $btnConfirm = $('<span class="datepicker--button-confirm">Применить</span>');
+$('.datepicker--buttons').append($btnConfirm);
+
+$('.datepicker--button-confirm').on('click', () => {
+    Datepicker.hide();
+});
+
+$('.date-filter').on('click', () => {
+    Datepicker.show();
+});
