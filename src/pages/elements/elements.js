64a9df7536d4a9b7da0c6b3f533c818770c98319
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-08-09 16:35:56 +0300

    Added elements in first coloumn

---
+++ src/pages/elements/elements.js
@@ -0,0 +1,8 @@
+import '../../components/textfield/textfield';
+import '../../components/dropdown/dropdown';
+import '../../components/inputmasked/inputmasked';
+import '../../components/date-dropdown/date-dropdown';
+import '../../components/date-filter/date-filter';
+import '../../components/subscription-textfield/subscription-textfield';
+
+import './elements.scss';
