3ecb3be436e602e2256912351e33eae6f902eac7
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-08-10 14:50:51 +0300

    Added elements in second coloumn

--- src/pages/elements/elements.js
+++ src/pages/elements/elements.js
@@ -4,5 +4,12 @@ import '../../components/inputmasked/inputmasked';
 import '../../components/date-dropdown/date-dropdown';
 import '../../components/date-filter/date-filter';
 import '../../components/subscription-textfield/subscription-textfield';
+import '../../components/checkbox-buttons/checkbox-buttons';
+import '../../components/radio-buttons/radio-buttons';
+import '../../components/toggle-buttons/toggle-buttons';
+import '../../components/like-block/like-block';
+import '../../components/rate-buttons/rate-buttons';
+import '../../components/richbox-list/richbox-list';
+import '../../components/range/range';
 
 import './elements.scss';
