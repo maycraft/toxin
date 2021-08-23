f2343e866154d046f6b7e6fe2f3cb24d373da77d
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-08-23 22:26:15 +0300

    Installed copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -15,7 +16,7 @@ module.exports = {
         cards: './src/pages/cards/cards.js',
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dest'),
         filename: '[name].[hash].bundle.js',
     },
     devServer: {
