793f297d0174321547424b341188fca4b48ddeff
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-09-30 17:24:17 +0300

    Created search page

---
+++ src/pages/search/search.js
@@ -0,0 +1,15 @@
+import '@scss/main.scss';
+import './search.scss';
+import '@components/header/header';
+import '@components/footer/footer';
+import '@components/date-dropdown/date-dropdown';
+import '@components/dropdown/dropdown';
+import '@components/range/range';
+import '@components/checkbox-buttons/checkbox-buttons';
+import '@components/richbox-list/richbox-list';
+import '@components/checkbox-list/checkbox-list';
+import '@components/card-room/card-room';
+import '@components/pagination/pagination';
+
+const $filterInst = $('#filter').data('datepicker');
+$filterInst.selectDate([new Date('2019-08-19'), new Date('2019-08-23')]);
