33fc77d6fba4dbb7bdee78467101e5f09c1b6297
Sergey Ryazantsev <rjazantsev@gmail.com>
2021-10-13 11:30:47 +0300

    Created registration page

---
+++ src/pages/registration/registration.js
@@ -0,0 +1,5 @@
+import '@scss/main.scss';
+import './registration.scss';
+import '@components/header/header';
+import '@components/footer/footer';
+import '@components/form-registration/form-registration';
